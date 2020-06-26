b532458b9f70b86939ffe22f9813fc8c4e8d2622
jay.r.reid@gmail.com <jay.r.reid@gmail.com>
2020-06-25 22:44:57 -0400

    removed quotes from variables

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ resource "aws_vpc" "this" {
 resource "aws_subnet" "public_subnet" {
   count             = length(var.public_subnet_cidr)
   vpc_id            = aws_vpc.this.id
-  cidr_block        = var.public_subnet_cidr[count.index]}
+  cidr_block        = var.public_subnet_cidr[count.index]
   availability_zone = data.aws_availability_zones.available.names[count.index]
 
   tags = "${
