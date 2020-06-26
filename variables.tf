f03794add62a8838459889b0bd4508947229dc9c
jay.r.reid@gmail.com <jay.r.reid@gmail.com>
2020-06-25 22:08:25 -0400

    updated variables.tf

--- variables.tf
+++ variables.tf
@@ -1,38 +1,41 @@
 #VPC
 variable "create_vpc" {
+  type = string
   default = " "
 }
 
 variable "cluster_name" {
+  type = string
   description = "Cluster name"
 }
 
 variable "vpc_name" {}
 
 variable "cidr" {
+  type = string
   description = "The CIDR block for the VPC."
 }
 
 #subnet variable
 variable "master_subnet_cidr" {
-  type        = "list"
+  type        = list
   description = "CIDR for master subnet"
   default     = []
 }
 
 variable "worker_subnet_cidr" {
-  type        = "list"
+  type        = list
   description = "CIDR for worker subnet"
   default     = []
 }
 
 variable "public_subnet_cidr" {
   description = "Kubernetes Public CIDR"
-  type        = "list"
+  type        = list
 }
 
 variable "private_subnet_cidr" {
-  type        = "list"
+  type        = list
   description = "Kubernetes Private CIDR"
   default     = []
 }
