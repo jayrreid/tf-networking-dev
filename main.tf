e4f6ae6d08d220fd6193d0fc248c5a7733399f04
jay.r.reid@gmail.com <jay.r.reid@gmail.com>
2020-06-25 22:54:00 -0400

    fixed tags for nat gateway

--- main.tf
+++ main.tf
@@ -124,9 +124,10 @@ resource "aws_nat_gateway" "nat-gw" {
   allocation_id = aws_eip.eip.id
   subnet_id     = element(aws_subnet.master_subnet.*.id, 0)
 
-  tags {
+  tags = {
     Name = "Nat_gateway"
   }
+
 }
 
 #Private Route Table
@@ -139,7 +140,7 @@ resource "aws_route_table" "rt_private" {
     nat_gateway_id = aws_nat_gateway.nat-gw.id
   }
 
-  tags {
+  tags = {
     Name = "Private_route_table"
   }
 }
